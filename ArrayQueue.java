1ee5a6dacab9303421416900de77c268f662d837
katejeongg <suhinjeong@gmail.com>
2024-09-09 00:17:27 -0400

    Update ArrayQueue.java

--- ArrayQueue.java
+++ ArrayQueue.java
@@ -1,29 +1,17 @@
 import java.util.NoSuchElementException;
 
 /**
- * Your implementation of an ArrayQueue.
- *
- * @author KATE JEONG
- * @version 1.0
- * @userid kjeong40
- * @GTID 903886263
- *
- * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
- *
- * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ * Implementation of an ArrayQueue.
  */
 public class ArrayQueue<T> {
 
     /*
      * The initial capacity of the ArrayQueue.
      *
-     * DO NOT MODIFY THIS VARIABLE.
      */
     public static final int INITIAL_CAPACITY = 9;
 
-    /*
-     * Do not add new instance variables or modify existing ones.
-     */
+
     private T[] backingArray;
     private int front;
     private int size;
@@ -38,11 +26,6 @@ public class ArrayQueue<T> {
     /**
      * Adds the data to the back of the queue.
      *
-     * If sufficient space is not available in the backing array, resize it to
-     * double the current length. When resizing, copy elements to the
-     * beginning of the new array and reset front to 0.
-     *
-     * Must be amortized O(1).
      *
      * @param data the data to add to the back of the queue
      * @throws IllegalArgumentException if data is null
@@ -68,15 +51,6 @@ public class ArrayQueue<T> {
     /**
      * Removes and returns the data from the front of the queue.
      *
-     * Do not shrink the backing array.
-     *
-     * Replace any spots that you dequeue from with null.
-     *
-     * If the queue becomes empty as a result of this call, do not reset
-     * front to 0.
-     *
-     * Must be O(1).
-     *
      * @return the data formerly located at the front of the queue
      * @throws java.util.NoSuchElementException if the queue is empty
      */
@@ -94,8 +68,6 @@ public class ArrayQueue<T> {
     /**
      * Returns the data from the front of the queue without removing it.
      *
-     * Must be O(1).
-     *
      * @return the data located at the front of the queue
      * @throws java.util.NoSuchElementException if the queue is empty
      */
@@ -109,39 +81,27 @@ public class ArrayQueue<T> {
     /**
      * Returns the front index of the queue.
      *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
      * @return the front index of the queue
      */
     public int getFront() {
-        // DO NOT MODIFY THIS METHOD!
         return front;
     }
 
     /**
      * Returns the backing array of the queue.
      *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
      * @return the backing array of the queue
      */
     public T[] getBackingArray() {
-        // DO NOT MODIFY THIS METHOD!
         return backingArray;
     }
 
     /**
      * Returns the size of the queue.
      *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
      * @return the size of the queue
      */
     public int size() {
-        // DO NOT MODIFY THIS METHOD!
         return size;
     }
 }
